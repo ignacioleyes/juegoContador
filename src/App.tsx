d6b4f4363dea1aefd1ee06291ac50195ecc99f57
Juan Ignacio Leyes Schneider <juanignacioleyes@gmail.com>
2023-07-07 11:51:10 -0300

    juegoContador finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import { useState } from "react";
-import { Button, Text, HStack, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Button, Text, HStack, VStack, Heading } from "@chakra-ui/react";
+import CountDown from "./CountDown";
 
 function App() {
     const [count, setCount] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
     const [disableClickMe, setDisableClickMe] = useState(true);
+    const [message, setMessage] = useState("");
 
     const handleIsPlaying = () => {
-        setCount(!isPlaying ? 0 : count);
         setIsPlaying(true);
+        setCount(0);
+
         setTimeout(() => {
-            setIsPlaying(false);
+            setMessage("Ready");
+        }, 1000);
+
+        setTimeout(() => {
+            setMessage("Set");
+        }, 2000);
+
+        setTimeout(() => {
+            setMessage("Go!");
             setDisableClickMe(false);
+        }, 3000);
+    };
+
+    const hadleStartGame = () => {
+        setTimeout(() => {
+            setMessage("");
+            setIsPlaying(false);
+            setDisableClickMe(true);
         }, 5000);
     };
 
     const handleClick = () => {
-        if (isPlaying) {
+        if (isPlaying && message === "Go!") {
             setCount(count + 1);
         }
     };
+
+    useEffect(() => {
+        if (message === "Go!") hadleStartGame();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [message]);
 
     return (
         <HStack w={"100vw"} h={"100vh"} p={0} m={0} spacing={0}>
@@ -30,9 +54,9 @@ function App() {
                 justifyContent={"center"}
             >
                 <Button
-                    disabled={isPlaying}
+                    isDisabled={isPlaying === true}
                     variant={"solid"}
-                    colorScheme={"teal"}
+                    colorScheme={"blackAlpha"}
                     size={"lg"}
                     onClick={handleIsPlaying}
                 >
@@ -42,14 +66,27 @@ function App() {
             <VStack
                 width={"50%"}
                 h={"100%"}
-                bg={"coral"}
+                bg={"whitesmoke"}
                 justifyContent={"center"}
             >
+                {message === "Go!" && <CountDown isPlaying />}
+                <Heading
+                    color={
+                        message === "Ready"
+                            ? "#bb1e10"
+                            : message === "Set"
+                            ? "#f7b500 "
+                            : "#32A431"
+                    }
+                >
+                    {message}
+                </Heading>
+
                 <Button
                     onClick={handleClick}
-                    disabled={disableClickMe}
+                    isDisabled={disableClickMe}
                     variant={"solid"}
-                    colorScheme={"teal"}
+                    colorScheme={"blackAlpha"}
                     size={"lg"}
                 >
                     Click Me!
---
+++ src/CountDown.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Center, Heading } from "@chakra-ui/react";
+
+interface Props {
+    isPlaying: boolean;
+}
+
+const CountDown = ({ isPlaying }: Props) => {
+    const [seconds, setSeconds] = useState(5);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(seconds - 1);
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [seconds]);
+
+    return (
+        <>
+            {isPlaying && (
+                <Heading
+                    bg="gray.400"
+                    borderRadius="50%"
+                    height="5rem"
+                    width="5rem"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    {seconds}
+                </Heading>
+            )}
+        </>
+    );
+};
+
+export default CountDown;
